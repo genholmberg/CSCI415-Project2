4a1531aae6e2594ff161e739175c3de459e47121
Michael Miller <Mike@Gabriels-MacBook-Pro.local>
2019-04-20 19:01:15 -0500

    added comments

--- md5test.cpp
+++ md5test.cpp
@@ -38,6 +38,12 @@ void init_array(uint4 T[]);
 string md5(string message);
 void uint_to_uchar(unsigned char digest[], uint4 state[]);
 
+/*
+* This method performs the MD5 crypt algorithm on a message.
+* Params: This method takes in a string as a parameter.
+* Pre: Message must already be defined.
+* Post: This method returns a hexadecimal string or the resulting hash code.
+*/
 string md5(string message){
 
 	string M[16];
@@ -45,6 +51,7 @@ string md5(string message){
 	uint4 T[64];
 	unsigned char digest[16];
 
+	// save the different states to be used during transformation
 	uint4 state[4];
 	state[0] = A;
 	state[1] = B;
@@ -53,25 +60,31 @@ string md5(string message){
 
 
 
-	string paddedMessage = pad_message(message);
+	string paddedMessage = pad_message(message);	// pad message
 
-	digestMessage(paddedMessage, M, X);
+	digestMessage(paddedMessage, M, X);	// break message into 16 32 bit words
 
-	init_array(T);
+	init_array(T);	// initialize array of 64 elements to be used in transformation
 
-	for(int i = 0; i < 1000; i++)
+	for(int i = 0; i < 1000; i++)	// slowdown
 	{
-		process_message(state, T, X);
+		process_message(state, T, X);	// perform transformation on message
 	}
 
-	uint_to_uchar(digest, state);
+	uint_to_uchar(digest, state);	// change uint4 to unsigned char
 
-  	string hashedMessage = hashCode(digest);
+  	string hashedMessage = hashCode(digest);	// get hash code
 
   return hashedMessage;
 
-}
+}// end md5
 
+/*
+* This method converts an unsigned int to unsigned char.
+* Params: This method takes in two arrays as parameters.
+* Pre: Both arrays must already be loaded with values.
+* Post: none.
+*/
 void uint_to_uchar(unsigned char digest[], uint4 state[]){
 
   for (int i = 0, j = 0; j < 16; i++, j += 4) {
@@ -79,28 +92,44 @@ void uint_to_uchar(unsigned char digest[], uint4 state[]){
     digest[j+1] = (state[i] >> 8) & 255;	// get second byte
     digest[j+2] = (state[i] >> 16) & 255;	// get third byte
     digest[j+3] = (state[i] >> 24) & 255;	// get fourth byte
-  }	
+  }	// end for
 
-}
+}// end uint_to_uchar
 
+/*
+* This method converts the character array to a hexidecimal hash code.
+* Params: This method takes in an array of unsigned char.
+* Pre: Array must be loaded with values.
+* Post: This method returns a 32 byte hexidecimal hash code.
+*/
 string hashCode(unsigned char digest[]){
 
   // output hash value
   char buffer[33];
   for(int i = 0; i < 16; i++)
-  	sprintf(buffer+i*2, "%02x", digest[i]);
+  	sprintf(buffer+i*2, "%02x", digest[i]);	// store hexidecimal value of character in buffer
   buffer[32] = 0;
 
   return string(buffer);
 
-}
+}// end hashCode
 
+
+/*
+* This method processes the message into 4 different rounds of transformations.
+* Params: This method takes in three arrays of type unsigned int.
+* Pre: Arrays must already be loaded with values.
+* Post: none.
+*/
 void process_message(uint4 state[], uint4 T[], uint4 X[]){
 
-	// Process Message
+	// store states into variable to be used in the transformation
+	// AA = A  BB = B  CC = C  DD = D
 	uint4 a = state[0], b = state[1], c = state[2], d = state[3];
 
 	//* Round 1 *//
+	// performs operation 
+	// a = b + ((a + F(b, c, d) + x[k] + t[i]) << s)
 	FF(a, b, c, d, 0, 7, 1, X, T);
 	FF(d, a, b, c, 1, 12, 2, X, T);
 	FF(c, d, a, b, 2, 17, 3, X, T);
@@ -119,6 +148,8 @@ void process_message(uint4 state[], uint4 T[], uint4 X[]){
 	FF(b, c, d, a, 15, 22, 16, X, T);
 
 	//* Round 2 */
+	// performs operation
+	// a = b + ((a + G(b, c, d) + x[k] + t[i]) << s)
 	GG(a, b, c, d, 1, 5, 17, X, T);
 	GG(d, a, b, c, 6, 9, 18, X, T);
 	GG(c, d, a, b, 11, 14, 19, X, T);
@@ -137,6 +168,8 @@ void process_message(uint4 state[], uint4 T[], uint4 X[]){
 	GG(b, c, d, a, 12, 20, 32, X, T);
 
 	//* Round 3 */
+	// performs operation
+	// a = b + ((a + H(b, c, d) + x[k] + t[i]) << s)
 	HH(a, b, c, d, 5, 4, 33, X, T);
 	HH(d, a, b, c, 8, 11, 34, X, T);
 	HH(c, d, a, b, 11, 16, 35, X, T);
@@ -155,6 +188,8 @@ void process_message(uint4 state[], uint4 T[], uint4 X[]){
 	HH(b, c, d, a, 2, 23, 48, X, T);
 
 	//* Round 4 */
+	// performs operation
+	// a = b + ((a + I(b, c, d) + x[k] + t[i]) << s)
 	II(a, b, c, d, 0, 6, 49, X, T);
 	II(d, a, b, c, 7, 10, 50, X, T);
 	II(c, d, a, b, 14, 15, 51, X, T);
@@ -172,14 +207,19 @@ void process_message(uint4 state[], uint4 T[], uint4 X[]){
 	II(c, d, a, b, 2, 15, 63, X, T);
 	II(b, c, d, a, 9, 21, 64, X, T);
 
-	state[0] += a;
-	state[1] += b;
-	state[2] += c;
-	state[3] += d;	
+	state[0] += a; 	// A = A + AA
+	state[1] += b;	// B = B + BB
+	state[2] += c;	// C = C + CC
+	state[3] += d;	// D = D + DD
 
-}
+}// end process_message
 
-
+/*
+* This method pads the message out to 512 bits.
+* Params: This method takes in a string as a parameter.
+* Pre: Message must already be defined.
+* Post: This method returns a binary string of the padded message.
+*/
 string pad_message(string message){
 
 	string binMessage = "";
@@ -188,7 +228,7 @@ string pad_message(string message){
 	// Get binary representation of Message
 	for(int i = 0; i < message.size(); i++){
 		binMessage += bitset<S>(message.c_str()[i]).to_string();
-	}
+	}// end for
 
 	// Append Padding Bits
 	if(binMessage.size() < LENGTH){
@@ -196,11 +236,11 @@ string pad_message(string message){
 
 		for(int i = binMessage.size(); i < LENGTH; i++){
 			binMessage += '0';
-		}
-	}
+		}// end for
+	}// end if
 
 
-	int m = message.size() << 3;
+	int m = message.size() << 3;	// size * 8
 
 	// Append Message Length
 	bitLength += bitset<64>(m).to_string();
@@ -209,8 +249,14 @@ string pad_message(string message){
 
 	return binMessage;
 
-}
+}// end pad_message
 
+/*
+* This method turns the binary message representation into 16 32 bit words.
+* Params: This method takes in a string, and two arrays as parameters.
+* Pre: String array must already be loaded and the message should be in binary.
+* Post: none.
+*/
 void digestMessage(string binMessage, string M[], uint4 X[]){
 
 	// Get 16 32 bit words
@@ -219,17 +265,23 @@ void digestMessage(string binMessage, string M[], uint4 X[]){
 		for(int j = 0; j < 32; j++){
 			M[i] += binMessage.substr()[index];
 			index++;
-		}
-	}
+		}// end for
+	}// end for
 
 	// string to unsigned int
 	for(int i = 0; i < 16; i++){
 		stringstream stream(M[i]);
 		stream >> X[i];
-	}
+	}// end for
 
-}
+}// end digestMessage
 
+/*
+* This method initializes the array of 64 elements to be used during transformation.
+* Params: This method takes in an unsigned int array as a parameter.
+* Pre: Array must be initialized.
+* Post: none.
+*/
 void init_array(uint4 T[]){
 
 	// T[i] = abs(sin(i+1) * 2^32)
@@ -238,38 +290,85 @@ void init_array(uint4 T[]){
 		int v = sin(i + 1);
 		v  = pow(2, 32);
 		T[i] = abs(v);
-	}	
-}
+	}// end for	
+}// end init_array
 
+/*
+* This method performs bitwise operations on three 32 bit words.
+* Params: This method takes in three 32 bit words as parameters.
+* Pre: all three words must be initialized.
+* Post: This method returns one 32 bit word.
+*/
 uint4 F(uint4 x, uint4 y, uint4 z){
 	return ((x & y) | (~x & z));
-}
+}// end F
 
-
+/*
+* This method performs bitwise operations on three 32 bit words.
+* Params: This method takes in three 32 bit words as parameters.
+* Pre: all three words must be initialized.
+* Post: This method returns one 32 bit word.
+*/
 uint4 G(uint4 x, uint4 y, uint4 z){
 	return ((x & z) | (y & ~z));
-}
+}// end G
 
+/*
+* This method performs bitwise operations on three 32 bit words.
+* Params: This method takes in three 32 bit words as parameters.
+* Pre: all three words must be initialized.
+* Post: This method returns one 32 bit word.
+*/
 uint4 H(uint4 x, uint4 y, uint4 z){
 	return ((x ^ y ^ z));
-}
+}// end H
 
+/*
+* This method performs bitwise operations on three 32 bit words.
+* Params: This method takes in three 32 bit words as parameters.
+* Pre: all three words must be initialized.
+* Post: This method returns one 32 bit word.
+*/
 uint4 I(uint4 x, uint4 y, uint4 z){
 	return (y ^ (x | ~z));
-}
+}// end I
 
+/*
+* This method performs the operation a = b + ((a + F(b, c, d) + x[k] + t[i]) << s).
+* Params: This method takes in 4 32 bit words, 3 integers, and two arrays.
+* Pre: all four words must be initialized, and the two arrays must be loaded.
+* Post: none.
+*/
 void FF(uint4 &a, uint4 b, uint4 c, uint4 d, int k, int s, int i, uint4 x[], uint4 t[]){
 	a = b + ((a + F(b, c, d) + x[k] + t[i]) << s);
-}
+}// end FF
 
+/*
+* This method performs the operation a = b + ((a + G(b, c, d) + x[k] + t[i]) << s).
+* Params: This method takes in 4 32 bit words, 3 integers, and two arrays.
+* Pre: all four words must be initialized, and the two arrays must be loaded.
+* Post: none.
+*/
 void GG(uint4 &a, uint4 b, uint4 c, uint4 d, int k, int s, int i, uint4 x[], uint4 t[]){
 	a = b + ((a + G(b, c, d) + x[k] + t[i]) << s);
-}
+}// end GG
 
+/*
+* This method performs the operation a = b + ((a + H(b, c, d) + x[k] + t[i]) << s).
+* Params: This method takes in 4 32 bit words, 3 integers, and two arrays.
+* Pre: all four words must be initialized, and the two arrays must be loaded.
+* Post: none.
+*/
 void HH(uint4 &a, uint4 b, uint4 c, uint4 d, int k, int s, int i, uint4 x[], uint4 t[]){
 	a = b + ((a + H(b, c, d) + x[k] + t[i]) << s);
-}
+}// end HH
 
+/*
+* This method performs the operation a = b + ((a + I(b, c, d) + x[k] + t[i]) << s).
+* Params: This method takes in 4 32 bit words, 3 integers, and two arrays.
+* Pre: all four words must be initialized, and the two arrays must be loaded.
+* Post: none.
+*/
 void II(uint4 &a, uint4 b, uint4 c, uint4 d, int k, int s, int i, uint4 x[], uint4 t[]){
 	a = b + ((a + I(b, c, d) + x[k] + t[i]) << s);
-}
+}// end II
